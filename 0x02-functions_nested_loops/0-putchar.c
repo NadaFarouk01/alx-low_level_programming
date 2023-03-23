9a312ab7a51e3b9f7694425896d1ba20b3d0da64
Nada <nadafarouk18@gmail.com>
2023-03-22 18:03:23 -0700

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
-* main - print _putchar
-* is written by mbah
-* Return: 0
-*/
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
 
 
 int main(void)
 {
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
@@ -17,6 +18,4 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
-
 }
-
